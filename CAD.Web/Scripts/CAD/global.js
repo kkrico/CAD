bb668220c3260318e8128e72dc78b2234679c229
Daniel Ramos <ramos.danielferreira@outlook.com>
2017-07-30 16:06:19 -0300

    Melhorado o estilo da página de login

--- CAD.Web/Scripts/CAD/global.js
+++ CAD.Web/Scripts/CAD/global.js
@@ -3,19 +3,19 @@
 });
 
 Eventos = {
-    ConfigurarValidation: function () {
+    ConfigurarValidation: function() {
 
         $(".validation-summary-errors").addClass("alert alert-danger");
         var forms = $("form");
 
-        forms.each(function (indice, form) {
+        forms.each(function(indice, form) {
 
             var validator = $(form).data("validator");
 
             var oldErrorPlacement = validator.settings.errorPlacement;
             var oldSuccess = validator.settings.success;
 
-            validator.settings.errorPlacement = function (error, el) {
+            validator.settings.errorPlacement = function(error, el) {
                 oldErrorPlacement(error, el);
                 if (error.text() !== "") {
                     el.closest(".form-group").addClass("has-error");
@@ -26,11 +26,10 @@ Eventos = {
                 }
             };
 
-            validator.settings.sucess = function (label, b) {
-                debugger;
+            validator.settings.sucess = function(label, b) {
                 label.parents('.form-group').removeClass('has-error');
                 oldSuccess(label);
-            }
+            };
         });
     }
-}
+};
